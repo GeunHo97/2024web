dee46626b3b8bc80cca93a9521b0d603d72e403b
GeunHo97 <rmsgh501@gmail.com>
2024-11-15 20:54:35 +0900

    homework11/15

--- src/day23/boardservice8mvc/model/BoardDao.java
+++ src/day23/boardservice8mvc/model/BoardDao.java
@@ -4,15 +4,26 @@ import java.util.ArrayList;
 
 public class BoardDao {
 
-    ArrayList<BoardDto> boardDb = new ArrayList<>()
+    //싱글톤
+    private static BoardDao boardDao = new BoardDao();
+    private BoardDao (){}
+    public static BoardDao getInstance(){
+        return boardDao;
+    }
 
-    //1. 게시물 등록 접근 함수
+
+    //여러개 게시물 저장하는 리스트
+    ArrayList<BoardDto> boardDB = new ArrayList<>();
+
+    // 1. 게시물 등록 접근 함수
     public boolean boardWrite( BoardDto boardDto){
-        boardDb.add( boardDto);
+        boardDB.add( boardDto );
         return true;
     }
 
-    public void boardPrint(){
-
+    // 2. 게시물 출력 접근 함수
+    public ArrayList<BoardDto> boardPrint( ){
+        return boardDB;
     }
+
 }
